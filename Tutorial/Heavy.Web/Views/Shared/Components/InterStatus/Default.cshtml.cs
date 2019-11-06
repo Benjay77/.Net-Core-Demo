85dc1ac01e0c3e8d5b4a6e803089b92e8763a364
Benjay·Shaw <benjay7@163.com>
2019-11-06 22:21:23 +0800

    taghelper and viewcomponents

---
+++ Tutorial/Heavy.Web/Views/Shared/Components/InterStatus/Default.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Heavy.Web.Views.Shared.ViewComponents.InterStatus
+{
+    public class DefaultModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
